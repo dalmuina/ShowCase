095f271e3920767034565194d9035f9e1d66eb5d
Diego <dalmuina@gmail.com>
2025-03-24 16:51:58 +0100

    clean code

--- app/src/main/java/com/dalmuina/showcase/games/presentation/navigation/NavManager.kt
+++ app/src/main/java/com/dalmuina/showcase/games/presentation/navigation/NavManager.kt
@@ -1,7 +1,6 @@
 package com.dalmuina.showcase.games.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
--- core/src/main/java/com/dalmuina/core/presentation/util/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.dalmuina.core.presentation.util
-
-object Constants {
-    const val URL_BASE = "https://api.rawg.io/api/"
-    const val ENDPOINT = "games"
-    const val API_KEY = "?key=37f4482fde834c2eacc917b929b0643d"
-}
