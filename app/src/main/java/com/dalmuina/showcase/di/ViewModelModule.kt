c5d38bb9d21a69f75dbc1141dd8e75bc62f8e26e
Diego <dalmuina@gmail.com>
2025-03-27 12:47:23 +0100

    Dispatcher injection

--- app/src/main/java/com/dalmuina/showcase/di/ViewModelModule.kt
+++ app/src/main/java/com/dalmuina/showcase/di/ViewModelModule.kt
@@ -1,9 +1,12 @@
 package com.dalmuina.showcase.di
 
 import com.dalmuina.showcase.games.presentation.viewmodel.GamesViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module{
+    single<CoroutineDispatcher> { Dispatchers.IO }
     viewModelOf(::GamesViewModel)
 }
